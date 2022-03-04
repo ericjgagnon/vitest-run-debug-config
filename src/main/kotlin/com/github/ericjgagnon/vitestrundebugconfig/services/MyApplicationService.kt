b463fc33edd182b1ad315165ff86e1c2a6bb08e7
GitHub Action <action@github.com>
2022-03-04 01:22:40 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ericjgagnon/vitestrundebugconfig/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ericjgagnon.vitestrundebugconfig.services
+
+import com.github.ericjgagnon.vitestrundebugconfig.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
